1810aa3f0ab4796b2676ca128e81293822dc930f
jywarren <jeff@unterbahn.com>
2016-12-30 15:07:47 -0500

    drag and drop

--- example/main.js
+++ example/main.js
@@ -1,31 +1,67 @@
 var distorter;
 
-(function() {
+jQuery(document).ready(function($) {
 
   distorter = FisheyeGl({
     image: 'images/grid.png'
   });
 
-  // mini fake jQuery:
-  function $(query){
-    return document.querySelector(query);
-  }
-
-  $("dl").addEventListener("change", function(e){
-    distorter.lens.a = $("#a_label").innerHTML = $("#a").value;
-    distorter.lens.b = $("#b_label").innerHTML = $("#b").value;
-    distorter.lens.F = $("#F_label").innerHTML = $("#F").value;
-    distorter.lens.scale = $("#scale_label").innerHTML = $("#scale").value;
-    distorter.fov.x = $("#fovx").value;
-    distorter.fov.y = $("#fovy").value;
+  $("dl").on("change", function(e){
+    distorter.lens.a = $("#a_label").innerHTML = $("#a").val();
+    distorter.lens.b = $("#b_label").innerHTML = $("#b").val();
+    distorter.lens.F = $("#F_label").innerHTML = $("#F").val();
+    distorter.lens.scale = $("#scale_label").innerHTML = $("#scale").val();
+    distorter.fov.x = $("#fovx").val();
+    distorter.fov.y = $("#fovy").val();
     distorter.run();
   });
 
-  $("#a").value = $("#a_label").innerHTML = distorter.lens.a;
-  $("#b").value = $("#b_label").innerHTML = distorter.lens.b;
-  $("#F").value = $("#F_label").innerHTML = distorter.lens.F;
-  $("#scale").value = $("#scale_label").innerHTML = distorter.lens.scale;
-  $("#fovx").value = distorter.fov.x;
-  $("#fovy").value = distorter.fov.y;
+  $("#a").val(distorter.lens.a);
+    $("#a_label").innerHTML = distorter.lens.a;
+  $("#b").val(distorter.lens.b);
+    $("#b_label").innerHTML = distorter.lens.b;
+  $("#F").val(distorter.lens.F);
+    $("#F_label").innerHTML = distorter.lens.F;
+  $("#scale").val(distorter.lens.scale);
+    $("#scale_label").innerHTML = distorter.lens.scale;
+  $("#fovx").val(distorter.fov.x);
+  $("#fovy").val(distorter.fov.y);
 
-})(this);
+
+  // Drag & Drop behavior
+
+  $('#canvas').on('dragenter',function(e) {
+    $('.zone').addClass('hover');
+  });
+
+  $('#canvas').on('dragleave',function(e) {
+    $('.zone').removeClass('hover');
+  });
+
+  var onDrop = function(e) {
+    e.preventDefault();
+    e.stopPropagation(); // stops the browser from redirecting.
+
+    var files = e.dataTransfer.files;
+    for (var i = 0, f; f = files[i]; i++) {
+      // Read the File objects in this FileList.
+
+      reader = new FileReader()
+      reader.onload = function(e) {
+        distorter.setImage(event.target.result);
+      }
+      reader.readAsDataURL(f);
+ 
+    }
+  }
+
+  function onDragOver(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+  }
+
+  $('#canvas').on('dragover', onDragOver, false);
+  $('#canvas')[0].addEventListener('drop', onDrop, false);
+
+});
